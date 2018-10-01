ce74cdfc7df885c9c7d4564428bca6fd4d120785
Sebastian <Sebastian@DESKTOP-IVCBMMU.uniandes.edu.co>
2018-10-01 16:17:08 -0500

    Clases SQL nuevas

--- src/uniandes/isis2304/supermercado/persistencia/SQLCliente.java
+++ src/uniandes/isis2304/supermercado/persistencia/SQLCliente.java
@@ -1,10 +1,12 @@
 package uniandes.isis2304.supermercado.persistencia;
 
 import java.math.BigDecimal;
+
 import java.util.List;
 import javax.jdo.PersistenceManager;
 import javax.jdo.Query;
 
+import uniandes.isis2304.supermercado.persistencia.*;
 class SQLCliente 
 {	
 	
@@ -16,6 +18,39 @@ class SQLCliente
 	 * Se renombra acá para facilitar la escritura de las sentencias
 	 */
 	private final static String SQL = PersistenciaSuperAndes.SQL;
+	/* ****************************************************************
+	 * 			Atributos
+	 *****************************************************************/
+	/**
+	 * El manejador de persistencia general de la aplicación
+	 */
+	private PersistenciaSuperAndes psa;
+	/* ****************************************************************
+	 * 			Métodos
+	 *****************************************************************/
+	/**
+	 * Constructor
+	 * @param pp - El Manejador de persistencia de la aplicación
+	 */
+	public SQLCliente(PersistenciaSuperAndes psa)
+	{
+		this.psa = psa;
+	}
 	
-	
+	/**
+	 * Crea y ejecuta la sentencia SQL para adicionar un CLIENTE a la base de datos de SuperAndes
+	 * @param pm - El manejador de persistencia
+	 * @param idCliente - El identificador del bebedor
+	 * @param nombre - El nombre del cliente
+	 * @param correo - El correo del cliente
+	 * @param ciudad - La ciudad del cliente
+	 * @param direccion - La dirección del cliente
+	 * @return EL número de tuplas insertadas
+	 */	
+	public long adicionarCliente(PersistenceManager pm, int idCliente, String nombre, String correo, String ciudad, String direccion)
+	{
+		Query sql = pm.newQuery(SQL, "INSERT INTO "+psa.darTablaCliente() + "(id, nombre, correo, ciudad, direccion) values (?, ?, ? , ?, ?)");
+		sql.setParameters(idCliente, nombre, correo, ciudad, direccion);
+		return (long) sql.executeUnique();
+	}
 }
