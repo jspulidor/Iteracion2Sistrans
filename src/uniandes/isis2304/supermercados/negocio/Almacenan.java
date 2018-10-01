4a1706b3272c83ddd8858661b8029017a3440914
Sebastian <Sebastian@DESKTOP-IVCBMMU.uniandes.edu.co>
2018-10-01 10:36:43 -0500

    Más clases añadidas

--- src/uniandes/isis2304/supermercados/negocio/Almacenan.java
+++ src/uniandes/isis2304/supermercados/negocio/Almacenan.java
@@ -1,5 +1,68 @@
 package uniandes.isis2304.supermercados.negocio;
 
-public class Almacenan {
+public class Almacenan 
+{
+	/* ****************************************************************
+	 * 			Atributos
+	 *****************************************************************/
+	
+	private int idProducto;
+	
+	private int idBodega;
+	
+	private int cantidadProducto;
+	
+	/* ****************************************************************
+	 * 			Métodos
+	 *****************************************************************/
+	
+	public Almacenan()
+	{
+		this.idProducto = 0;
+		this.idBodega = 0;
+		this.cantidadProducto = 0;
+	}
+	
+	public Almacenan(int pIdProducto, int pIdBodega, int pCantidadProductos)
+	{
+		this.idProducto = pIdProducto;
+		this.idBodega = pIdBodega;
+		this.cantidadProducto = pCantidadProductos;
+	}
+
+	public int getIdProducto()
+	{
+		return idProducto;
+	}
+	
+	public void setIdProducto(int pIdProducto)
+	{
+		this.idProducto = pIdProducto;
+	}
+	
+	public int getIdBodega()
+	{
+		return idBodega;
+	}
+	
+	public void setIdBodega(int pIdBodega)
+	{	
+		this.idBodega = pIdBodega;
+	}
+	
+	public int getCantidadProductos()
+	{
+		return cantidadProducto;
+	}
+	
+	public void setCantidadProductos(int pCantidadProductos)
+	{
+		this.cantidadProducto = pCantidadProductos;
+	}
+	
+	public String toString()
+	{
+		return "Almacenan [idProducto =" +idProducto+ ", idBodega =" +idBodega+ ", cantidad =" +cantidadProducto+ "]";
+	}
 
 }
