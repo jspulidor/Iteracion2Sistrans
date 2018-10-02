a2dcb6e7986c23a77036560abf3c293e3521a673
jsnumpaque10 <js.numpaque10@uniandes.edu.co>
2018-10-02 14:34:36 -0500

    xd

--- src/uniandes/isis2304/supermercado/interfazApp/InterfazSupermercadoApp.java
+++ src/uniandes/isis2304/supermercado/interfazApp/InterfazSupermercadoApp.java
@@ -572,37 +572,7 @@ public class InterfazSupermercadoApp extends JFrame implements ActionListener {
 		panelDatos.actualizarInterfaz(resultado);
 	}
 	
-	/**
-	 * Limpia todas las tuplas de todas las tablas de la base de datos de parranderos
-	 * Muestra en el panel de datos el número de tuplas eliminadas de cada tabla
-	 */
-	public void limpiarBD ()
-	{
-		try 
-		{
-    		// Ejecución de la demo y recolección de los resultados
-			long eliminados [] = superAndes.limpiarSuperAndes();
-			
-			// Generación de la cadena de caracteres con la traza de la ejecución de la demo
-			String resultado = "\n\n************ Limpiando la base de datos ************ \n";
-			resultado += eliminados [0] + " Gustan eliminados\n";
-			resultado += eliminados [1] + " Sirven eliminados\n";
-			resultado += eliminados [2] + " Visitan eliminados\n";
-			resultado += eliminados [3] + " Bebidas eliminadas\n";
-			resultado += eliminados [4] + " Tipos de bebida eliminados\n";
-			resultado += eliminados [5] + " Bebedores eliminados\n";
-			resultado += eliminados [6] + " Bares eliminados\n";
-			resultado += "\nLimpieza terminada";
-   
-			panelDatos.actualizarInterfaz(resultado);
-		} 
-		catch (Exception e) 
-		{
-//			e.printStackTrace();
-			String resultado = generarMensajeError(e);
-			panelDatos.actualizarInterfaz(resultado);
-		}
-	}
+
     
 	/* ****************************************************************
 	 * 			Métodos privados para la presentación de resultados y otras operaciones
--- src/uniandes/isis2304/supermercados/negocio/SuperAndes.java
+++ src/uniandes/isis2304/supermercados/negocio/SuperAndes.java
@@ -4,6 +4,7 @@ import org.apache.log4j.Logger;
 
 import com.google.gson.JsonObject;
 
+import uniandes.isis2304.parranderos.negocio.TipoBebida;
 import uniandes.isis2304.supermercado.persistencia.PersistenciaSuperAndes;
 
 
@@ -53,23 +54,49 @@ public class SuperAndes
 	{
 		psa.cerrarUnidadPersistencia ();
 	}
+
+	public VOCliente adicionarCliente(int idCliente, String nombre, String correo, String ciudad) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public VOSucursal adicionarSucursal(String ciudad, String sector, String direccion) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public VOEstante adicionarBodega(int tipoProducto, int sucursal, Double capacidadVolumen, Double capacidadPeso) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public VOEstante adicionarEstante(int tipoProducto, int sucursal, Double capacidadVolumen, Double capacidadPeso,
+			int nivelAbastecimiento) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public TipoProducto adicionarTipoProducto(String nombreTipo) {
+		
+        log.info ("Adicionando Tipo de bebida: " + nombreTipo);
+        TipoProducto tipoBebida = psa.adicionarTipoPropucto(nombreTipo);		
+        log.info ("Adicionando Tipo de bebida: " + tipoBebida);
+        return tipoBebida;
+		
+	}
+
+	public VOProveedor adicionarProveedor(int nit, String nombre, Double calificacionCalidad) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public VOProducto adicionarProducto(String codigoBarras, int tipoProducto, String nombre, String marca,
+			String presentacion, String unidadMedida, Double cantidadPresentacion, Double pesoEmpaque,
+			Double volumenEmpaque) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 
-	/* ****************************************************************
-	 * 			Métodos para administración
-	 *****************************************************************/
-
-	/**
-	 * Elimina todas las tuplas de todas las tablas de la base de datos de Parranderos
-	 * @return Un arreglo con 7 números que indican el número de tuplas borradas en las tablas GUSTAN, SIRVEN, VISITAN, BEBIDA,
-	 * TIPOBEBIDA, BEBEDOR y BAR, respectivamente
-	 */
-	public long [] limpiarSuperAndes ()
-	{
-        log.info ("Limpiando la BD de Parranderos");
-        long [] borrrados = psa.limpiarSuperAndes();	
-        log.info ("Limpiando la BD de Parranderos: Listo!");
-        return borrrados;
-	}
 
 }
